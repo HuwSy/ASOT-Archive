f75c18d632b0aa760da5c21d5044af5840337b42
Huw <HuwSy@users.noreply.github.com>
2016-03-11 07:15:13 +0000

    Updated log name

--- ASOT.sh
+++ ASOT.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-if [ ! -f ./Asot.log ]; then
+if [ ! -f ./ASOT.log ]; then
   echo "New log"‎
-  echo 0 > Asot.log
+  echo 0 > ASOT.log
 fi
 EP=`curl -v --silent http://www.asotarchive.org --stderr - | grep '[/]episode[/][\?]p=[0-9]*' -o | head -n 2 | tail -n 1`
 CU=`echo $EP | grep -o [0-9]*`
-LA=`cat Asot.log`
+LA=`cat ASOT.log`
 echo "Current episode"
 echo $CU
 echo "Last download"
@@ -14,7 +14,7 @@ if [ "$LA" \< "$CU" ]; then
   echo "New download"‎
   RD=`curl -v --silent ‎http://www.asotarchive.org$EP --stderr - | grep 'Location: http' | grep -o http[:/a-zA-Z0-9\.-]*`‎
   DL=`curl -v --silent $RD --stderr - | grep -o 'http[^"]*\.mp3'`
-  echo $CU > Asot.log
+  echo $CU > ASOT.log
   cd downloads
   wget -c $DL
 fi
